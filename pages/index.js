55a51937692d142ede2f0291a3263df99b686a8b
Alex Burke <33670882+manitu85@users.noreply.github.com>
2021-09-03 23:28:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h2 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
