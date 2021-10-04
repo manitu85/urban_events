dd0742b37c4e130287158d217bccf02f9c797331
Alex Burke <alexburke.dev@gmail.com>
2021-10-04 14:05:05 +0200

    #

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-         <Link href='/about'>About</Link>
+      <Link href='/about'>About</Link>
     </div>
   )
 }
