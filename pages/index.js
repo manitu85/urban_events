a0f1652109b7a406fc16b8af5e25cbc35c60f989
Alex Burke <alexburke.dev@gmail.com>
2021-12-05 00:46:05 +0100

    #

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout';
 export default function HomePage() {
 	return (
 		<Layout>
-			<h1 name='check'>Home Page</h1>
+			<h3 name='check'>Home Page</h3>
 		</Layout>
 	);
 }
