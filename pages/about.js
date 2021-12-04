49764059f926412edf03c793fd5a95beeea2eff5
Alex Burke <alexburke.dev@gmail.com>
2021-12-05 00:57:39 +0100

    #

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout';
 export default function About() {
 	return (
 		<Layout>
-			<h1>About</h1>
+			<h3>About</h3>
 			<p>This is an app to find the latest DJ and other musical events</p>
 			<p>Version: 1.0.0.</p>
 		</Layout>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout';
 export default function HomePage() {
 	return (
 		<Layout>
-			<h3 name='check'>Home Page</h3>
+			<h1 name='check'>Home Page</h1>
 		</Layout>
 	);
 }
