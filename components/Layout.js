4c607b1f2fd029f5911d6bdb6b090fd4e476f7a9
Alex Burke <alexburke.dev@gmail.com>
2021-12-05 18:46:36 +0100

    style: add layout style

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,9 @@ import Head from 'next/head';
 
 import styles from '@/styles/Layout.module.scss';
 
+import Footer from './Footer';
+import Header from './Header';
+
 export default function Layout({ title, keywords, description, children }) {
 	return (
 		<div>
@@ -10,13 +13,15 @@ export default function Layout({ title, keywords, description, children }) {
 				<meta name='description' content={description} />
 				<meta name='keyword' content={keywords} />
 			</Head>
+			<Header />
 			<div className={styles.container}>{children}</div>
+			<Footer />
 		</div>
 	);
 }
 
 Layout.defaultProps = {
-	title: 'DJ Events | Find the hottest parties  ',
+	title: 'Urbanbug | DJ Events | Find the hottest parties  ',
 	description: 'Find the latest DJ and other musical events',
 	keywords: 'music, dj, techno, trance, goth, edm, events',
 };
