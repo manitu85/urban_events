095889a59c51480339b4a222f8b754323768c12f
Alex Burke <alexburke.dev@gmail.com>
2021-12-05 18:48:07 +0100

    feat: add footer

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+import styles from '@/styles/Footer.module.scss';
+
+export default function Footer() {
+	return (
+		<footer className={styles.footer}>
+			<p>Copyright &copy: Urbanbug | DJ Events 2021</p>
+			<p>
+				<Link href='/about'>
+					<a>About the project</a>
+				</Link>
+			</p>
+		</footer>
+	);
+}
