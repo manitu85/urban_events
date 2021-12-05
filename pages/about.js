e7c7733afde84bdde3074840357f7c45fbc3b0d4
Alex Burke <alexburke.dev@gmail.com>
2021-12-05 02:12:50 +0100

    feat: changed headings colors

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout';
 export default function About() {
 	return (
 		<Layout>
-			<h3>About</h3>
+			<h1>About</h1>
 			<p>This is an app to find the latest DJ and other musical events</p>
 			<p>Version: 1.0.0.</p>
 		</Layout>
