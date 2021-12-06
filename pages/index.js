aa94b4035e6208476fa07665f19e66a8c9fb2a03
Alex Burke <alexburke.dev@gmail.com>
2021-12-06 12:29:57 +0100

    feat: preparing event components for strapi api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import Layout from '@/components/Layout';
+import Link from 'next/link';
 
-export default function HomePage() {
+import EventItem from '@/components/EventItem';
+import Layout from '@/components/Layout';
+import API_URL from '@/config/index';
+
+export default function HomePage({ events }) {
 	return (
 		<Layout>
-			<h1 name='check'>Home Page</h1>
+			<h1>Upcoming Events</h1>
+			{events.length === 0 && <h3>No events to show</h3>}
+			{events.map(evt => (
+				<EventItem key={evt.key} evt={evt} />
+			))}
+			{events.length > 0 && (
+				<Link href='/events'>
+					<a className='btn-secondary'>View All Events</a>
+				</Link>
+			)}
 		</Layout>
 	);
 }
+
+// export async function getStaticProps() {
+// 	const res = await fetch(`${API_URL}/api/events`);
+// 	const events = res.json();
+
+// 	return {
+// 		props: { events },
+// 		revalidate: 1,
+// 	};
+// }
