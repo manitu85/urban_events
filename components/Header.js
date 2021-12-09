921407835d875b808680bc0a158639d8ac25526e
Alex Burke <alexburke.dev@gmail.com>
2021-12-09 18:14:57 +0100

    feat: add auth routes

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
+import { useContext } from 'react';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/Header.module.scss';
 
 import Search from './Search';
 
 export default function Header() {
+	const { user, logout } = useContext(AuthContext);
 	return (
 		<header className={styles.header}>
 			<div className={styles.logo}>
 				<Link href='/'>
-					<a>Urbanbug | DJ Events</a>
+					<a>Urban Ants </a>
 				</Link>
 			</div>
 			<Search />
@@ -20,11 +24,39 @@ export default function Header() {
 							<a>Events</a>
 						</Link>
 					</li>
-					<li>
-						<Link href='/events/add'>
-							<a>Add Event</a>
-						</Link>
-					</li>
+					{user ? (
+						// If logged in
+						<>
+							<li>
+								<Link href='/events/add'>
+									<a>Add Event</a>
+								</Link>
+							</li>
+							<li>
+								<Link href='/account/dashboard'>
+									<a>Dashboard</a>
+								</Link>
+							</li>
+							<li>
+								<button
+									type='button'
+									onClick={() => logout()}
+									className='btn-secondary btn-icon'
+								>
+									<FaSignOutAlt /> Logout
+								</button>
+							</li>
+						</>
+					) : (
+						// If logged out
+						<li>
+							<Link href='/account/login'>
+								<a className='btn-secondary btn-icon'>
+									<FaSignInAlt /> Login
+								</a>
+							</Link>
+						</li>
+					)}
 				</ul>
 			</nav>
 		</header>
