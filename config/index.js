60bd1cc84da958edccb6e2592902c631cf91dec7
Alex Burke <alexburke.dev@gmail.com>
2021-12-09 18:17:43 +0100

    chore: add new config variable

--- config/index.js
+++ config/index.js
@@ -1,5 +1,11 @@
+// Frontend
+export const NEXT_URL =
+	process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+
+// Backend
 export function getStrapiURL(path = '' || ``) {
 	return `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'}${path}`;
 }
 
+// Pages
 export const PER_PAGE = 2;
