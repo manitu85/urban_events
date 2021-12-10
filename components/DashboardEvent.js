34df360e05194e094c567ff98b279154132db109
Alex Burke <alexburke.dev@gmail.com>
2021-12-10 02:06:02 +0100

    feat: created single dasboard event

---
+++ components/DashboardEvent.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+
+import styles from '@/styles/DashboardEvent.module.scss';
+
+export default function DashboardEvent({ evt, handleDelete }) {
+	return (
+		<div className={styles.event}>
+			<h4>
+				<Link href={`/events/${evt.slug}`}>
+					<a>{evt.name}</a>
+				</Link>
+			</h4>
+			<Link href={`/events/edit/${evt.id}`}>
+				<a className={styles.edit}>
+					<FaPencilAlt /> <span>Edit Event</span>
+				</a>
+			</Link>
+			{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+			<a
+				href='#'
+				className={styles.delete}
+				onClick={() => handleDelete(evt.id)}
+			>
+				<FaTimes /> <span>Delete</span>
+			</a>
+		</div>
+	);
+}
