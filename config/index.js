f594981823f75633b94eed4b1754a57aff7c31cb
Alex Burke <alexburke.dev@gmail.com>
2021-12-12 22:07:05 +0100

    fix: fix

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ export const NEXT_URL =
 	process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
 
 // Backend
-export function getStrapiURL(path = '') {
+export function getStrapiURL(path = `` || '') {
 	return `${
 		process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
 	}${path}`;
