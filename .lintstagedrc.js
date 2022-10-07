07310d6becd6587a2a9e751c106b20f2f9823fef
Alex Burke <alexburke.dev@gmail.com>
2022-10-07 22:13:08 +0200

    test: fix lintstaged file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,26 +1,28 @@
 const path = require('path');
 
-// Fix: eslint next js
+// Fix: Next js eslint
 const buildEslintCommand = filenames =>
 	`yarn lint:fix ${filenames
 		.map(f => path.relative(process.cwd(), f))
 		.join(' ')}`;
 
-const prettierCheck = 'yarn prettier';
-
+// Format
 const buildFormatCommand = filenames =>
 	filenames.map(filename => `yarn format '${filename}'`);
 
-// Format: MarkDown, JSON, Js, Jsx
+const prettierCheck = 'yarn prettier';
+
 module.exports = {
 	'**/*.{js,jsx}': buildEslintCommand,
 	'**/*.{js,jsx,json,md,mdx}': [prettierCheck, buildFormatCommand],
 };
 
-// Bug: Next js recommended with --file params on Windows WSL2: Ubuntu doesn't work
+// Bug: Next js recommended lint-staged with --file param on Windows WSL2: Ubuntu doesn't work, probably the path is wrong
 // module.exports = {
 // 	'**/*.js?(x)': filenames =>
-// 		`yarn lint:fix --file ${filenames
+// 		`next lint --cache --fix --file ${filenames
 // 			.map(file => file.split(process.cwd())[1])
 // 			.join(' --file ')}`,
 // };
+
+// https://nextjs.org/docs/basic-features/eslint#lint-staged
