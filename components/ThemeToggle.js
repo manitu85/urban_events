9a15f8f9c789bc3be3913f6a63747ca83b974ff2
Alex Burke <alexburke.dev@gmail.com>
2022-10-11 22:40:52 +0200

    test: test branch name

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -8,7 +8,7 @@ const ThemeToggle = () => {
 		<IconButton
 			onClick={toggleColorMode}
 			icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunLine />}
-			aria-label='theme switcher'
+			aria-label='theme toggler'
 			variant='outline'
 			_hover={{ background: 'none' }}
 		/>
